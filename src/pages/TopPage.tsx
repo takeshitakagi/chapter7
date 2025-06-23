9773fa348de2b1151684d56bcdbc4a6d2a722b9e
takeshitakagi <takagitakeshi2@gmail.com>
2025-06-23 17:15:15 +0900

    読み込み中をisLoadingのstateで定義する

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -3,13 +3,16 @@ import type { Post } from '../types/Post';
 
 import PostCard from './PostCard';
 import { Link } from 'react-router-dom';
+import { tr } from 'zod/v4/locales';
 
 function TopPage() {
 
-  const [posts, setPosts] = useState<Post[] | null>(null);
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetcher = async () => {
+      setIsLoading(true);
       try {
         const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts");
         if (!res.ok) {
@@ -20,12 +23,14 @@ function TopPage() {
       } catch (error) {
         console.log("記事の取得に失敗しました;", error);
         setPosts([]);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetcher();
   }, [])
 
-  if (posts === null) {
+  if (isLoading) {
     return <div>記事を読み込み中...</div>
   }
 
